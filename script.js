7e4dfec14ff91860a7a7ddee6f326390567e4ecd
seaboyz <seaboyz@hotmail.com>
2021-10-22 13:10:03 -0500

    Finished desktop version

--- script.js
+++ script.js
@@ -4,15 +4,24 @@ const navMenu = document.querySelector('.nav__menu');
 navBtn.addEventListener('click', () => {
 	if (navBtn.classList.contains('hamburger')) {
 		navBtn.classList.replace('hamburger', 'close');
+		navMenu.style.display = 'block';
 	} else if (navBtn.classList.contains('close')) {
 		navBtn.classList.replace('close', 'hamburger');
+		navMenu.style.display = 'none';
 	}
-	navMenu.classList.toggle('hidden');
 });
 
-document.querySelectorAll('.sub-menu-title').forEach(_ =>
+document.querySelectorAll('.nav__sub-menu').forEach(_ => {
 	_.addEventListener('click', e => {
-		e.currentTarget.children[0].classList.toggle('arrow--up');
-		e.currentTarget.nextElementSibling.classList.toggle('hidden');
-	})
-);
+		const subMenuList = e.currentTarget.children[1];
+		const arrow = e.currentTarget.children[0].children[0];
+		// toggle sub menu
+		subMenuList.style.display === 'flex'
+			? (subMenuList.style.display = 'none')
+			: (subMenuList.style.display = 'flex');
+		// rotate arrow
+		arrow.style.transform === 'rotate(180deg)'
+			? (arrow.style.transform = 'unset')
+			: (arrow.style.transform = 'rotate(180deg)');
+	});
+});
